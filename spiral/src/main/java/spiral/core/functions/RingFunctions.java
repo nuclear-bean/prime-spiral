e5bf475b52d3081b2560da63ca430205e345fca4
jakubpartyka <kontakt@jakub-partyka.pl>
2023-04-01 21:52:57 +0200

    docs

--- spiral/src/main/java/spiral/core/functions/RingFunctions.java
+++ spiral/src/main/java/spiral/core/functions/RingFunctions.java
@@ -5,10 +5,18 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class RingFunctions {
 
+    /**
+     * @param n ring number
+     * @return nuber of values on a given ring
+     */
     public int calculateRingCapacity(int n) {
         return n * 8;
     }
 
+    /**
+     * @param n ring number
+     * @return smallest value on a given ring
+     */
     public long calculateRingMinValue(int n) {
         long val = (2L * n) - 1;
         val = val * val;
