3420800ce99d87c6c79f17c299d4a838f36b877a
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-21 12:44:25 +0000

    DensityTranslator implemented

---
+++ spiral/src/main/java/spirals/ulam/translators/DensityTranslator.java
@@ -0,0 +1,44 @@
+package spirals.ulam.translators;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Accepts binary matrix and translates it to density matrix. Density is calculated as sum of all 8 neighbours and self. Each prime neighbour is counted as 1, each non-prime as 0.
+ */
+@UtilityClass
+public class DensityTranslator {
+
+    public long[][] translate(boolean[][] matrix) {
+        long[][] densityMatrix = new long[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < densityMatrix[i].length; j++) {
+                densityMatrix[i][j] = calculateDensity(matrix, i, j);
+            }
+        }
+        return densityMatrix;
+    }
+
+    private static long calculateDensity(boolean[][] matrix, int i, int j) {
+        long density = 0;
+
+        density += getDensity(matrix, i - 1, j - 1); // top left
+        density += getDensity(matrix, i - 1, j); // top
+        density += getDensity(matrix, i - 1, j + 1); // top right
+        density += getDensity(matrix, i, j - 1); // left
+        density += getDensity(matrix, i, j); // self
+        density += getDensity(matrix, i, j + 1); // right
+        density += getDensity(matrix, i + 1, j - 1); // bottom left
+        density += getDensity(matrix, i + 1, j); // bottom
+        density += getDensity(matrix, i + 1, j + 1); // bottom right
+
+        return density;
+    }
+
+    private static long getDensity(boolean[][] matrix, int i, int j) {
+        try {
+            return matrix[i][j] ? 1 : 0;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return 0;
+        }
+    }
+}
---
+++ spiral/src/test/java/spirals/ulam/translators/DensityTranslatorTest.java
@@ -0,0 +1,31 @@
+package spirals.ulam.translators;
+
+import org.junit.jupiter.api.Test;
+import spirals.ulam.generators.SimpleUlamGenerator;
+import utils.ConsolePrinter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DensityTranslatorTest {
+
+    @Test
+    public void testDensityTranslation_1() {
+        long [][] input = SimpleUlamGenerator.generateMatrix(3);
+        boolean[][] booleans = BinaryTranslator.translateToBoolean(input);
+        long [][] expected = {{1, 3, 2}, {2, 4, 2}, {1, 2, 1}};
+        long [][] actual = DensityTranslator.translate(booleans);
+        ConsolePrinter.prettyPrintMatrixWithFrames(input);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testDensityTranslation_2() {
+        long [][] input = SimpleUlamGenerator.generateMatrix(5);
+        boolean[][] booleans = BinaryTranslator.translateToBoolean(input);
+        long [][] expected = {{2, 2, 2, 2, 2}, {3, 3, 3, 4, 4}, {3, 3, 4, 3, 3}, {2, 3, 3, 3, 2}, {1, 2, 2, 1, 0}};
+        long [][] actual = DensityTranslator.translate(booleans);
+        ConsolePrinter.prettyPrintMatrixWithFrames(input);
+        assertArrayEquals(expected, actual);
+    }
+
+}
