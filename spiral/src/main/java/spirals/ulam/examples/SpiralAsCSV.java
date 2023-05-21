4d469d3c8ba4e0b43ec4a33e70f30c33724b25e0
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-21 13:21:27 +0000

    README.md update

--- spiral/src/main/java/spirals/ulam/examples/SpiralAsCSV.java
+++ spiral/src/main/java/spirals/ulam/examples/SpiralAsCSV.java
@@ -6,12 +6,15 @@ import spirals.ulam.generators.SimpleUlamGenerator;
 
 import java.io.IOException;
 
+/**
+ * Creates basic Ulam spiral and saves it as csv. Output file is a matrix of given size filled with 0s and 1s where 1 represents prime number and 0 represents non-prime number.
+ */
 @Log
 public class SpiralAsCSV extends AbstractExample {
 
     public static void main(String[] args) throws IOException {
-        PATH = "ulam_spiral_1001.csv";
-        SIZE = 1001;
+        SIZE = 10_001;
+        PATH = "ulam_spiral_" + SIZE + ".csv";
         start(args);
 
         log.info("generating matrix...");
