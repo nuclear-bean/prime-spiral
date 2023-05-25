a0688b74379c9c61355e9fc6a1961c84b843d24b
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-25 20:30:30 +0000

    javadoc added to MatrixContentOperations

--- spiral/src/main/java/utils/matrix/MatrixContentOperations.java
+++ spiral/src/main/java/utils/matrix/MatrixContentOperations.java
@@ -37,10 +37,26 @@ public final class MatrixContentOperations {
         return countOfTrueCellsWithinRadius(matrix, i, j, radius);
     }
 
+    /**
+     * Returns number of true cells placed on a diagonals within a given radius from a specified point.
+     * @param matrix matrix to retrieve elements from
+     * @param i row index of the specified point
+     * @param j column index of the specified point
+     * @param radius radius from the specified point
+     * @return number of true cells placed on a diagonals within a given radius from a specified point
+     */
     public static int getDiagonalCountOfTrueCellsWithinRadius(boolean[][] matrix, int i, int j, int radius) {
         return diagonalCountOfTrueCellsWithinRadius(matrix, i, j, radius);
     }
 
+    /**
+     * Returns number of true cells placed on axial within a given radius from a specified point.
+     * @param matrix matrix to retrieve elements from
+     * @param i row index of the specified point
+     * @param j column index of the specified point
+     * @param radius radius from the specified point
+     * @return number of true cells placed on an axial within a given radius from a specified point
+     */
     public static int getAxialCountOfTrueCellWithinRadius(boolean[][] matrix, int i, int j, int radius) {
         return get_axial_count_of_true_cells_within_radius(matrix, i, j, radius);
     }
