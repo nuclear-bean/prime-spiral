8c27de03130362e2d227866daf5b573c28001a52
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-25 20:26:58 +0000

    MatrixUtils class added

---
+++ spiral/src/main/java/utils/matrix/MatrixUtils.java
@@ -0,0 +1,14 @@
+package utils.matrix;
+
+import lombok.NonNull;
+
+public class MatrixUtils {
+
+    public static boolean safeIsTrue(@NonNull boolean[][] matrix, int i, int j) {
+        try {
+            return matrix[i][j];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+}
---
+++ spiral/src/test/java/utils/matrix/MatrixUtilsTest.java
@@ -0,0 +1,75 @@
+package utils.matrix;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MatrixUtilsTest {
+
+    @Test
+    public void testSafeIsTrue_withinBoundsFalse() {
+        boolean[][] matrix = {
+                {true, false, true},
+                {false, true, false},
+                {true, true, false}
+        };
+
+        boolean result = MatrixUtils.safeIsTrue(matrix, 1, 2);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testSafeIsTrue_withinBoundsTrue() {
+        boolean[][] matrix = {
+                {true, false, true},
+                {false, true, false},
+                {true, true, false}
+        };
+
+        boolean result = MatrixUtils.safeIsTrue(matrix, 0, 0);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testSafeIsTrue_outOfBounds() {
+        boolean[][] matrix = {
+                {true, false},
+                {false, true}
+        };
+
+        boolean result = MatrixUtils.safeIsTrue(matrix, 2, 1);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testSafeIsTrue_emptyMatrix() {
+        boolean[][] matrix = {};
+
+        boolean result = MatrixUtils.safeIsTrue(matrix, 0, 0);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testSafeIsTrue_onBorder() {
+        boolean[][] matrix = {
+                {true, false, true},
+                {false, true, false},
+                {true, true, false}
+        };
+
+        boolean result = MatrixUtils.safeIsTrue(matrix, 0, 1);
+
+        Assertions.assertFalse(result);
+    }
+
+    @SuppressWarnings({"ConstantConditions", "ResultOfMethodCallIgnored"})
+    @Test
+    public void testSafeIsTrue_nullMatrix() {
+        boolean[][] matrix = null;
+
+        Assertions.assertThrows(NullPointerException.class, () -> MatrixUtils.safeIsTrue(matrix, 0, 0));
+    }
+}
