d4e674b27651094db7d2dc428df65978d4611408
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-25 18:10:19 +0000

    progress report logging added to density translator

--- spiral/src/main/java/spirals/ulam/translators/density/ImprovedDensityTranslator.java
+++ spiral/src/main/java/spirals/ulam/translators/density/ImprovedDensityTranslator.java
@@ -1,18 +1,32 @@
 package spirals.ulam.translators.density;
 
+import lombok.extern.log4j.Log4j2;
 import utils.MatrixElementsWithinRadiusUtils;
 
+import java.time.Instant;
+
+@Log4j2
 public class ImprovedDensityTranslator {
+
+    private static Instant lastProgressReport = Instant.MIN;
 
     public static short[][] translate(DensitySpecification specification) {
         int matrixSize = specification.getMatrix().length;
         short[][] densityMatrix = new short[matrixSize][matrixSize];
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
+                reportProgress(i, matrixSize);
                 densityMatrix[i][j] = calculateDensity(specification, i, j);
             }
         }
         return densityMatrix;
+    }
+
+    private static void reportProgress(int i, int matrixSize) {
+        if (lastProgressReport.plusSeconds(10).isBefore(Instant.now())) {
+            log.info("calculating density... {}%", (i * 100) / matrixSize);
+            lastProgressReport = Instant.now();
+        }
     }
 
     private static short calculateDensity(DensitySpecification specification, int i, int j) {
