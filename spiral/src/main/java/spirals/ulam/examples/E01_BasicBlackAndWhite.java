16f356b32176d8b985fafc1a1d2e77a809ae7ac3
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-26 21:17:21 +0000

    E01_BasicBlackAndWhite nice refactor

--- spiral/src/main/java/spirals/ulam/examples/E01_BasicBlackAndWhite.java
+++ spiral/src/main/java/spirals/ulam/examples/E01_BasicBlackAndWhite.java
@@ -2,9 +2,14 @@ package spirals.ulam.examples;
 
 import lombok.extern.log4j.Log4j2;
 import spirals.ulam.examples.abstracts.AbstractExample;
-import spirals.ulam.export.image.BlackAndWhiteImageExporter;
+import spirals.ulam.export.image.BinaryImageGenerator;
 import spirals.ulam.generators.SimpleUlamGenerator;
-import utils.ElapsedTimer;
+import spirals.ulam.translators.generic.GenericTranslator;
+import spirals.ulam.translators.generic.MatrixElementMappingFunction;
+
+import static spirals.ulam.translators.BinaryTranslator.translateToBoolean;
+import static utils.matrix.MatrixUtils.unwrap;
+import static utils.matrix.MatrixUtils.wrap;
 
 /**
  * Creates basic Ulam spiral and saves it as spirals.ulam.export.image. Primes are mapped to black pixels, non-primes to white.
@@ -13,26 +18,21 @@ import utils.ElapsedTimer;
 public class E01_BasicBlackAndWhite extends AbstractExample {
 
     public static void main(String[] args) {
-        setup();
-        long[][] matrix = generateMatrix();
-        generateImage(matrix);
+        long[][] matrix = SimpleUlamGenerator.generateMatrix(501);
+        boolean [][] primeMapping = translateToBoolean(matrix);
+        MatrixElementMappingFunction mappingFunction = defineMatrixElementMappingFunction();
+        GenericTranslator translator = new GenericTranslator();
+        Short[][] result = translator.translate(wrap(primeMapping), mappingFunction);
+        BinaryImageGenerator.generateImage(unwrap(result), "generic.png");
     }
 
-    private static void generateImage(final long[][] matrix) {
-        log.info("generating spirals.ulam.export.image...");
-        BlackAndWhiteImageExporter.generateImage(matrix, PATH);
-        log.info("generation completed");
-    }
-
-    private static long[][] generateMatrix() {
-        log.info("generating matrix...");
-        return SimpleUlamGenerator.generateMatrix(SIZE);
-    }
-
-    private static void setup() {
-        log.info("starting...");
-        ElapsedTimer.start();
-        SIZE = 5001;
-        PATH = "ulam_spiral_" + SIZE + ".png";
+    private static MatrixElementMappingFunction defineMatrixElementMappingFunction() {
+        return (i, j, primeMap) -> {
+            if (primeMap[i][j]) {
+                return (short) 1;
+            } else {
+                return (short) 0;
+            }
+        };
     }
 }
