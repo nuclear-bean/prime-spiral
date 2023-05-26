749172e74c65224a64eabe2b5d8169dc8f1002f4
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-26 21:48:43 +0000

    SimplifiedAbstractExample - allow method override

--- spiral/src/main/java/spirals/ulam/examples/abstracts/SimplifiedAbstractExample.java
+++ spiral/src/main/java/spirals/ulam/examples/abstracts/SimplifiedAbstractExample.java
@@ -23,29 +23,29 @@ public abstract class SimplifiedAbstractExample {
         generateImage(matrixMapping);
     }
 
-    private void prepare() {
+    protected void prepare() {
         log.info("Preparing...");
         ElapsedTimer.start();
     }
 
-    private void generateImage(Short[][] matrixMapping) {
-        log.info("Generating image...");
-        BinaryImageGenerator.generateImage(unwrap(matrixMapping), "generic.png");      //todo filename generation
+    protected long[][] generateMatrix() {
+        log.info("Generating matrix...");
+        return SimpleUlamGenerator.generateMatrix(501);
     }
 
-    private Short[][] getMapping(boolean[][] primeMapping, MatrixMappingFunction mappingFunction) {
-        log.info("Generating mapping...");
-        return GenericTranslator.translate(wrap(primeMapping), mappingFunction);
-    }
-
-    private boolean[][] getPrimeMapping(long[][] matrix) {
+    protected boolean[][] getPrimeMapping(long[][] matrix) {
         log.info("Generating prime mapping...");
         return BinaryTranslator.translateToBoolean(matrix);
     }
 
-    private long[][] generateMatrix() {
-        log.info("Generating matrix...");
-        return SimpleUlamGenerator.generateMatrix(501);
+    protected Short[][] getMapping(boolean[][] primeMapping, MatrixMappingFunction mappingFunction) {
+        log.info("Generating mapping...");
+        return GenericTranslator.translate(wrap(primeMapping), mappingFunction);
+    }
+
+    protected void generateImage(Short[][] matrixMapping) {
+        log.info("Generating image...");
+        BinaryImageGenerator.generateImage(unwrap(matrixMapping), "generic.png");      //todo filename generation
     }
 
     protected abstract MatrixMappingFunction defineMatrixMappingFunction();
