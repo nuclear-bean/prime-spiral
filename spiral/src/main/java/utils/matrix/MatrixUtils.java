c4ed00501decab95dd0a22730e3922edc50bdcfd
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-26 20:16:31 +0000

    mapToShort mapping method added

--- spiral/src/main/java/utils/matrix/MatrixUtils.java
+++ spiral/src/main/java/utils/matrix/MatrixUtils.java
@@ -11,4 +11,17 @@ public class MatrixUtils {
             return false;
         }
     }
+
+    /**
+     * Converts a matrix of primitive shorts to a matrix of boxed Shorts.
+     */
+    public static Short[][] mapToShort(short [][] input) {
+        Short[][] result = new Short[input.length][input.length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                result[i][j] = input[i][j];
+            }
+        }
+        return result;
+    }
 }
