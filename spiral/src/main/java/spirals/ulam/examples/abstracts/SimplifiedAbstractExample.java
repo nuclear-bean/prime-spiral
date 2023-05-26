e05f33f5552faf27d54c544a8d45a2b0874cc5da
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-26 21:35:55 +0000

    beautify SimplifiedAbstractExample

--- spiral/src/main/java/spirals/ulam/examples/abstracts/SimplifiedAbstractExample.java
+++ spiral/src/main/java/spirals/ulam/examples/abstracts/SimplifiedAbstractExample.java
@@ -1,22 +1,51 @@
 package spirals.ulam.examples.abstracts;
 
+import lombok.extern.log4j.Log4j2;
 import spirals.ulam.export.image.BinaryImageGenerator;
 import spirals.ulam.generators.SimpleUlamGenerator;
+import spirals.ulam.translators.BinaryTranslator;
 import spirals.ulam.translators.generic.GenericTranslator;
 import spirals.ulam.translators.generic.MatrixMappingFunction;
+import utils.ElapsedTimer;
 
-import static spirals.ulam.translators.BinaryTranslator.translateToBoolean;
 import static utils.matrix.MatrixUtils.unwrap;
 import static utils.matrix.MatrixUtils.wrap;
 
+@Log4j2
 public abstract class SimplifiedAbstractExample {
 
     protected void run() {
-        long[][] matrix = SimpleUlamGenerator.generateMatrix(501);
-        boolean [][] primeMapping = translateToBoolean(matrix);
+        prepare();
+        long[][] matrix = generateMatrix();
+        boolean[][] primeMapping = getPrimeMapping(matrix);
         MatrixMappingFunction mappingFunction = defineMatrixMappingFunction();
-        Short[][] result = GenericTranslator.translate(wrap(primeMapping), mappingFunction);
-        BinaryImageGenerator.generateImage(unwrap(result), "generic.png");      //todo filename generation
+        Short[][] matrixMapping = getMapping(primeMapping, mappingFunction);
+        generateImage(matrixMapping);
+    }
+
+    private void prepare() {
+        log.info("Preparing...");
+        ElapsedTimer.start();
+    }
+
+    private void generateImage(Short[][] matrixMapping) {
+        log.info("Generating image...");
+        BinaryImageGenerator.generateImage(unwrap(matrixMapping), "generic.png");      //todo filename generation
+    }
+
+    private Short[][] getMapping(boolean[][] primeMapping, MatrixMappingFunction mappingFunction) {
+        log.info("Generating mapping...");
+        return GenericTranslator.translate(wrap(primeMapping), mappingFunction);
+    }
+
+    private boolean[][] getPrimeMapping(long[][] matrix) {
+        log.info("Generating prime mapping...");
+        return BinaryTranslator.translateToBoolean(matrix);
+    }
+
+    private long[][] generateMatrix() {
+        log.info("Generating matrix...");
+        return SimpleUlamGenerator.generateMatrix(501);
     }
 
     protected abstract MatrixMappingFunction defineMatrixMappingFunction();
