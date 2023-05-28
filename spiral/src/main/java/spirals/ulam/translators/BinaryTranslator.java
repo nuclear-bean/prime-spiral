2e78887f8a3484465c9376f0ec513ae058d6db7a
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-28 19:33:58 +0000

    unused method removed

--- spiral/src/main/java/spirals/ulam/translators/BinaryTranslator.java
+++ spiral/src/main/java/spirals/ulam/translators/BinaryTranslator.java
@@ -7,20 +7,6 @@ import math.PrimeUtils;
  */
 public final class BinaryTranslator {
 
-    public static long[][] translate(long[][] matrix) {
-        long[][] binaryMatrix = new long[matrix.length][matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < binaryMatrix[i].length; j++) {
-                if (PrimeUtils.isPrime(matrix[i][j])) {
-                    binaryMatrix[i][j] = 1;
-                } else {
-                    binaryMatrix[i][j] = 0;
-                }
-            }
-        }
-        return binaryMatrix;
-    }
-
     /**
      * @return binary matrix. Primes are mapped to true, non-primes to false.
      */
--- spiral/src/test/java/spirals/ulam/translators/BinaryTranslatorTest.java
+++ spiral/src/test/java/spirals/ulam/translators/BinaryTranslatorTest.java
@@ -7,22 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinaryTranslatorTest {
 
     @Test
-    public void testTranslation_1() {
-        long[][] input = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        long[][] expected = {{0, 1, 1}, {0, 1, 0}, {1, 0, 0}};
-        long[][] actual = BinaryTranslator.translate(input);
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void testTranslation_2() {
-        long[][] input = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        long[][] expected = {{0, 1, 1, 0}, {1, 0, 1, 0}, {0, 0, 1, 0}, {1, 0, 0, 0}};
-        long[][] actual = BinaryTranslator.translate(input);
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
     public void testTranslateToBoolean_1() {
         long[][] input = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         boolean[][] expected = {{false, true, true}, {false, true, false}, {true, false, false}};
