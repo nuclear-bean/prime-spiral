23a7cb3778d91ac2e5ebda571770222427d61cad
jakubpartyka <kontakt@jakub-partyka.pl>
2023-05-28 18:51:49 +0000

    deprecated classes removed

--- spiral/src/main/java/spirals/ulam/examples/abstracts/AbstractExample.java
+++
@@ -1,9 +0,0 @@
-package spirals.ulam.examples.abstracts;
-
-@Deprecated(forRemoval = true)
-public class AbstractExample {
-
-    protected static int SIZE = 15001;
-    protected static String PATH;
-
-}
