a81acaa7a7750fd5c0be62472cb79e99d1d8caa5
jakubpartyka <kontakt@jakub-partyka.pl>
2023-06-05 15:54:06 +0000

    E03 refactor

--- spiral/src/main/java/spirals/ulam/examples/E03_ExportToSlimCSV.java
+++ spiral/src/main/java/spirals/ulam/examples/E03_ExportToSlimCSV.java
@@ -2,7 +2,9 @@ package spirals.ulam.examples;
 
 import lombok.extern.log4j.Log4j2;
 import spirals.ulam.examples.abstracts.AbstractExample;
+import spirals.ulam.export.csv.BasicCSVExporter;
 import spirals.ulam.export.csv.SlimCSVExporter;
+import spirals.ulam.generators.SimpleUlamGenerator;
 import spirals.ulam.translators.generic.MatrixMappingFunction;
 import utils.export.OutputPathProvider;
 import matrix.MatrixMapper;
@@ -13,33 +15,11 @@ import java.io.IOException;
  * Creates basic Ulam spiral and saves it as csv. Output file is a matrix of given size filled with 0s and 1s where 1 represents prime number and 0 represents non-prime number.
  */
 @Log4j2
-public class E03_ExportToSlimCSV extends AbstractExample {
+public class E03_ExportToSlimCSV {
 
-    public static void main(String[] args) {
-        int size = 501;
-        String outputPath = OutputPathProvider.getOutputPath("slim", size, ".csv", E03_ExportToSlimCSV.class);
-        new E03_ExportToSlimCSV().run(size, outputPath);
-    }
-
-    @Override
-    protected MatrixMappingFunction defineMatrixMappingFunction() {
-        return (i,j,primeMap) -> {
-            if (primeMap[i][j]) {
-                return (short) 1;
-            } else {
-                return (short) 0;
-            }
-        };
-    }
-
-    @Override
-    protected void generateImage(short[][] matrixMapping, String outputPath) {
-        try {
-            log.info("Generating csv...");
-            boolean[][] primeMap = MatrixMapper.mapToBoolean(matrixMapping);
-            SlimCSVExporter.generateCSV(primeMap, outputPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        long[][] matrix = SimpleUlamGenerator.generateMatrix(501);
+        boolean[][] booleanMapping = MatrixMapper.mapToBoolean(matrix);
+        SlimCSVExporter.generateCSV(booleanMapping, OutputPathProvider.getOutputPath("ulam_slim", matrix.length, ".csv", E03_ExportToSlimCSV.class));
     }
 }
