844791a881451037409e01d35ea446ea57fb8eb9
jakubpartyka <kontakt@jakub-partyka.pl>
2023-06-05 15:52:29 +0000

    E02 refactor

--- spiral/src/main/java/matrix/MatrixMapper.java
+++ spiral/src/main/java/matrix/MatrixMapper.java
@@ -1,6 +1,7 @@
 package matrix;
 
 import lombok.NoArgsConstructor;
+import math.PrimeUtils;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public final class MatrixMapper {
@@ -16,6 +17,20 @@ public final class MatrixMapper {
             }
         }
         return longMatrix;
+    }
+
+    /**
+     * @return boolean matrix. Primes are mapped to true, composites to false.
+     * @throws IllegalArgumentException if matrix contains anything but 1s and 0s
+     */
+    public static boolean[][] mapToBoolean(long[][] matrix) {
+        boolean[][] binaryMatrix = new boolean[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < binaryMatrix[i].length; j++) {
+                binaryMatrix[i][j] = PrimeUtils.isPrime(matrix[i][j]);
+            }
+        }
+        return binaryMatrix;
     }
 
     /**
--- spiral/src/main/java/spirals/ulam/examples/E02_ExportToCSV.java
+++ spiral/src/main/java/spirals/ulam/examples/E02_ExportToCSV.java
@@ -1,33 +1,20 @@
 package spirals.ulam.examples;
 
 import lombok.extern.log4j.Log4j2;
-import spirals.ulam.examples.abstracts.AbstractExample;
-import spirals.ulam.export.csv.BasicCSVExporter;
-import spirals.ulam.translators.generic.MatrixMappingFunction;
-import utils.export.OutputPathProvider;
 import matrix.MatrixMapper;
+import spirals.ulam.export.csv.BasicCSVExporter;
+import spirals.ulam.generators.SimpleUlamGenerator;
+import utils.export.OutputPathProvider;
 
 /**
  * Creates basic Ulam spiral and saves it as csv. Output file is a matrix of given size filled with 0s and 1s where 1 represents prime number and 0 represents non-prime number.
  */
 @Log4j2
-public class E02_ExportToCSV extends AbstractExample {
+public class E02_ExportToCSV {
 
     public static void main(String[] args) {
-        int size = 5_001;
-        String outputPath = OutputPathProvider.getOutputPath("csv", size, ".csv", E02_ExportToCSV.class);
-        new E02_ExportToCSV().run(size, outputPath);
-    }
-
-    @Override
-    protected MatrixMappingFunction defineMatrixMappingFunction() {
-        return (i, j, primeMap) -> primeMap[i][j] ? (short) 1 : (short) 0;
-    }
-
-    @Override
-    protected void generateImage(short[][] matrixMapping, String outputPath) {
-        log.info("Generating csv...");
-        boolean[][] primeMap = MatrixMapper.mapToBoolean(matrixMapping);
-        BasicCSVExporter.generateCSV(primeMap, outputPath);
+        long[][] matrix = SimpleUlamGenerator.generateMatrix(1001);
+        boolean[][] booleanMapping = MatrixMapper.mapToBoolean(matrix);
+        BasicCSVExporter.generateCSV(booleanMapping, OutputPathProvider.getOutputPath("ulam", matrix.length, ".csv", E02_ExportToCSV.class));
     }
 }
