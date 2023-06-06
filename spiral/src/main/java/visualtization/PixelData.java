df7164ffd9c08494785c6d94ef96fcaf486e8c3f
jakubpartyka <kontakt@jakub-partyka.pl>
2023-06-06 20:04:38 +0000

    PixelData - javadoc + more constants

--- spiral/src/main/java/visualtization/PixelData.java
+++ spiral/src/main/java/visualtization/PixelData.java
@@ -4,16 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a single pixel in an image.
+ */
 @AllArgsConstructor
 @Getter
 @Setter
 public class PixelData {
 
+    public static PixelData BLACK = new PixelData(0, 0, 0);
+    public static PixelData WHITE = new PixelData(255, 255, 255);
+
     public static PixelData RED = new PixelData(255, 0, 0);
     public static PixelData GREEN = new PixelData(0, 255, 0);
     public static PixelData BLUE = new PixelData(0, 0, 255);
-    public static PixelData BLACK = new PixelData(0, 0, 0);
-    public static PixelData WHITE = new PixelData(255, 255, 255);
+
+    public static PixelData MAGENTA = new PixelData(255, 0, 255);
+    public static PixelData YELLOW = new PixelData(255, 255, 0);
+    public static PixelData CYAN = new PixelData(0, 255, 255);
 
     private int red;
     private int green;
