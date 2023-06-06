3d49ec9c35e352238c69a7487dd108a832ccad41
jakubpartyka <kontakt@jakub-partyka.pl>
2023-06-06 20:08:44 +0000

    cosmetics

--- spiral/src/main/java/translation/BasicPDTLogger.java
+++ spiral/src/main/java/translation/BasicPDTLogger.java
@@ -4,13 +4,13 @@ import lombok.NonNull;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-class BasicPDTLogger extends Thread {
+final class BasicPDTLogger extends Thread {
 
     private final int matrixSize;
     private final int matrixElementsCount;
     private final PixelDataTranslator subject;
 
-    public BasicPDTLogger(final int matrixSize, @NonNull final PixelDataTranslator subject) {
+    BasicPDTLogger(final int matrixSize, @NonNull final PixelDataTranslator subject) {
         this.matrixSize = matrixSize;
         this.subject = subject;
         this.matrixElementsCount = matrixSize * matrixSize;
