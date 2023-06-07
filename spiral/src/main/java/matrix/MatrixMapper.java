9b0363141b3fcf748c6b7476a66b18519e77a718
jakubpartyka <kontakt@jakub-partyka.pl>
2023-06-07 22:01:13 +0000

    unused code removed

--- spiral/src/main/java/matrix/MatrixMapper.java
+++ spiral/src/main/java/matrix/MatrixMapper.java
@@ -32,23 +32,4 @@ public final class MatrixMapper {
         }
         return binaryMatrix;
     }
-
-    /**
-     * @return boolean matrix. 1s are mapped to true, 0s to false.
-     * @throws IllegalArgumentException if matrix contains anything but 1s and 0s
-     */
-    public static boolean[][] mapToBoolean(short[][] matrix) {
-        boolean[][] binaryMatrix = new boolean[matrix.length][matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < binaryMatrix[i].length; j++) {
-                binaryMatrix[i][j] = switch (matrix[i][j]) {
-                    case 1 -> binaryMatrix[i][j] = true;
-                    case 0 -> binaryMatrix[i][j] = false;
-                    default -> throw new IllegalArgumentException("Only 1s and 0s are allowed");
-                };
-            }
-        }
-        return binaryMatrix;
-    }
-
 }
--- spiral/src/test/java/matrix/MatrixMapperTest.java
+++ spiral/src/test/java/matrix/MatrixMapperTest.java
@@ -1,6 +1,5 @@
 package matrix;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,35 +13,4 @@ class MatrixMapperTest {
         long[][] actual = MatrixMapper.shorts2longs(input);
         assertArrayEquals(expected, actual);
     }
-
-    @Test
-    void testMapToBoolean_AllOnes() {
-        short[][] matrix = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-        boolean[][] expected = {{true, true, true}, {true, true, true}, {true, true, true}};
-        boolean[][] actual = MatrixMapper.mapToBoolean(matrix);
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void testMapToBoolean_AllZeros() {
-        short[][] matrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-        boolean[][] expected = {{false, false, false}, {false, false, false}, {false, false, false}};
-        boolean[][] actual = MatrixMapper.mapToBoolean(matrix);
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void testMapToBoolean_MixedValues() {
-        short[][] matrix = {{1, 0, 1}, {0, 1, 0}, {1, 1, 0}};
-        boolean[][] expected = {{true, false, true}, {false, true, false}, {true, true, false}};
-        boolean[][] actual = MatrixMapper.mapToBoolean(matrix);
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void testMapToBoolean_InvalidValue() {
-        short[][] matrix = {{1, 2, 0}, {1, 1, 0}, {1, 0, 1}};
-        Assertions.assertThrows(IllegalArgumentException.class, () -> MatrixMapper.mapToBoolean(matrix));
-    }
-
 }
